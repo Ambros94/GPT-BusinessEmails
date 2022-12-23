02809f5bfd1c28d6dc7d56070966fc0dfa25999c
Ambros <luca.ambrosini@pepper.zone>
2022-12-23 23:06:14 +0100

    Fix silly stuff

--- content.js
+++ content.js
@@ -1,20 +1,20 @@
-
 //LISTENERS
 // Listen to scroll event
 window.addEventListener("scroll", function () {
-  var x = window.scrollX, y = window.scrollY;
-  for (var i = 0; i < popUpShadow.listOfUnpinnedPopups.length; i++) {
-    var id = popUpShadow.listOfUnpinnedPopups[i];
-    elem = popUpShadow.shadowRoot.getElementById(parseInt(id));
-    var elemTop = elem.offsetTop - (y - this.window.lastY);
-    var elemLeft = elem.offsetLeft - (x - this.window.lastX);
+    const x = window.scrollX, y = window.scrollY;
+    let elem;
+    for (let i = 0; i < popUpShadow.listOfUnpinnedPopups.length; i++) {
+        const id = popUpShadow.listOfUnpinnedPopups[i];
+        elem = popUpShadow.shadowRoot.getElementById(parseInt(id));
+        const elemTop = elem.offsetTop - (y - this.window.lastY);
+        const elemLeft = elem.offsetLeft - (x - this.window.lastX);
 
-    elem.style.top = elemTop + 'px';
-    elem.style.left = elemLeft + 'px';
-  }
-  this.window.lastY = y;
-  this.window.lastX = x;
-  });
+        elem.style.top = elemTop + 'px';
+        elem.style.left = elemLeft + 'px';
+    }
+    this.window.lastY = y;
+    this.window.lastX = x;
+});
 
 
 const getSelectedText = () => window.getSelection().toString();
@@ -22,55 +22,57 @@ const getSelectedText = () => window.getSelection().toString();
 document.addEventListener("contextmenu", () => {
     // console.log(getSelectedText().length)
     if (getSelectedText().length > 0) {
-      setmousePosition("mousePosition_primary", getMarkerPosition());
+        setMousePosition("mousePosition_primary", getMarkerPosition());
     }
-  });
-  
+});
+
 document.addEventListener('contextmenu', function (e) {
     var mousePos = getMousePosition(e);
-    setmousePosition("mousePosition_support", {
-      left: mousePos.x,
-      top: mousePos.y,
+    setMousePosition("mousePosition_support", {
+        left: mousePos.x,
+        top: mousePos.y,
     });
-  });
+});
 
 function addListenersForDrag() {
     // add a listener to the mouse down event, to call the mouseDown function, to each popup in the shadowDOM
-    for (var i = 0; i < popUpShadow.listOfActivePopups.length; i++) {
-      //
-      var id = popUpShadow.listOfActivePopups[i];
-      elem = popUpShadow.shadowRoot.getElementById(id + "header").addEventListener('mousedown', mouseDown, false);
+    for (let i = 0; i < popUpShadow.listOfActivePopups.length; i++) {
+        //
+        const id = popUpShadow.listOfActivePopups[i];
+        popUpShadow.shadowRoot.getElementById(id + "header").addEventListener('mousedown', mouseDown, false);
     }
-  }
-  
+}
+
 function spanMove(e, id_target) {
-    var simpleid = id_target.replace('header', '')
-    var fullpopup = popUpShadow.shadowRoot.getElementById(simpleid)
-    var header = popUpShadow.shadowRoot.getElementById(id_target)
-  
-    var mouse_y_position = e.clientY - header.offsetHeight / 2 - 20; //20 is the radius of the border I think
-    var mouse_x_position = e.clientX - fullpopup.offsetWidth / 2;
-  
-    fullpopup.style.top = mouse_y_position + 'px';
-    fullpopup.style.left = mouse_x_position + 'px';
-  }
-  
+    let simple = id_target.replace('header', '');
+    let fullPopup = popUpShadow.shadowRoot.getElementById(simple);
+    let header = popUpShadow.shadowRoot.getElementById(id_target);
+
+    let mouse_y_position = e.clientY - header.offsetHeight / 2 - 20; //20 is the radius of the border I think
+    let mouse_x_position = e.clientX - fullPopup.offsetWidth / 2;
+
+    fullPopup.style.top = mouse_y_position + 'px';
+    fullPopup.style.left = mouse_x_position + 'px';
+}
+
 function mouseDown(e) {
     // this is to avoid the selection of the child text, when the target is the parent
     // if (e.target.id == this.id+'text')
     //   return;
     e.preventDefault(); // prevent the selection of the text below the popup
     const id_target = this.id;
+
     function wrapper(event) {
-      spanMove(event, id_target)
+        spanMove(event, id_target)
     }
+
     window.addEventListener('mousemove', wrapper, true);
     // add a listener to the mouse up event, to remove the wrapper function when the mouse is up
     window.addEventListener('mouseup', function () {
-      window.removeEventListener('mousemove', wrapper, true);
-    }
-      , false);
-  }
+            window.removeEventListener('mousemove', wrapper, true);
+        }
+        , false);
+}
 
 
 // POPUP FUNCTIONS
@@ -80,132 +82,111 @@ document.body.appendChild(popUpShadow); //attach the shadowDOM to body
 
 // MOUSE POSITION FUNCTIONS
 function getMousePosition(e) {
-  var posx = 0;
-  var posy = 0;
-  if (!e) var e = window.event;
-  if (e.clientX || e.clientY) {
-    posx = e.clientX
-    posy = e.clientY
-  }
-  return {
-    x: posx,
-    y: posy
-  }
+    let posX = 0;
+    let posY = 0;
+    if (!e) let e = window.event;
+    if (e.clientX || e.clientY) {
+        posX = e.clientX
+        posY = e.clientY
+    }
+    return {
+        x: posX,
+        y: posY
+    }
 }
-const setmousePosition = (name_attribute, mousePosition) =>
-  popUpShadow.setAttribute(
-    name_attribute,
-    JSON.stringify(mousePosition)
-  );
 
+const setMousePosition = (name_attribute, mousePosition) =>
+    popUpShadow.setAttribute(
+        name_attribute,
+        JSON.stringify(mousePosition)
+    );
 
 
 function getMarkerPosition() {
-  const rangeBounds = window
-    .getSelection()
-    .getRangeAt(0)
-    .getBoundingClientRect();
-  return {
-    left: rangeBounds.right + 5,
-    top: rangeBounds.top,
-  };
+    const rangeBounds = window
+        .getSelection()
+        .getRangeAt(0)
+        .getBoundingClientRect();
+    return {
+        left: rangeBounds.right + 5,
+        top: rangeBounds.top,
+    };
 }
 
 
 // function that set mousePosition to the popup
-function setmousePositionToPopup() {
-  if (popUpShadow.hasAttribute("mousePosition_primary")) {
-    popUpShadow.setAttribute("mousePosition", popUpShadow.getAttribute("mousePosition_primary"));
-    //remove the attribute mousePosition_primary
-    popUpShadow.removeAttribute("mousePosition_primary");
-  }
-  else if (popUpShadow.hasAttribute("mousePosition_support")) {
-    popUpShadow.setAttribute("mousePosition", popUpShadow.getAttribute("mousePosition_support"));
-    //remove the attribute mousePosition_support
-    popUpShadow.removeAttribute("mousePosition_support");
-  }
-  else { popUpShadow.setAttribute("mousePosition", JSON.stringify({ left: 0, top: 0 })) }
+function setMousePositionToPopup() {
+    if (popUpShadow.hasAttribute("mousePosition_primary")) {
+        popUpShadow.setAttribute("mousePosition", popUpShadow.getAttribute("mousePosition_primary"));
+        //remove the attribute mousePosition_primary
+        popUpShadow.removeAttribute("mousePosition_primary");
+    } else if (popUpShadow.hasAttribute("mousePosition_support")) {
+        popUpShadow.setAttribute("mousePosition", popUpShadow.getAttribute("mousePosition_support"));
+        //remove the attribute mousePosition_support
+        popUpShadow.removeAttribute("mousePosition_support");
+    } else {
+        popUpShadow.setAttribute("mousePosition", JSON.stringify({left: 0, top: 0}))
+    }
 }
 
 
 function checkIdPopup(id) {
-  if (id == undefined || id == -1) {
-    return popUpShadow.ids
-  }
-  else {
-    return parseInt(id)
-  }
+    if (id === undefined || id === -1) {
+        return popUpShadow.ids
+    } else {
+        return parseInt(id)
+    }
 }
 
 
-
 chrome.runtime.onMessage.addListener(function (request) {
-  //  if attribute message in request exists, it's a gpt-3 response
-  var id_popup = checkIdPopup(request.id_popup);
-  // check if request.uuid exists
- 
-  if (request.message == 'showPopUp') {
-    popUpShadow.ids++; // increment the number of popups, and id of the new popup
-    popUpShadow.listOfActivePopups.push(popUpShadow.ids);
-    popUpShadow.listOfUnpinnedPopups.push(popUpShadow.ids);
-    setmousePositionToPopup();
-    popUpShadow.defaultpopup(); // show the popup
-    addListenersForDrag();
-  }
-  else if (request.message == 'showPopUpOnTheFly') {
-    popUpShadow.ids++;
-    popUpShadow.listOfActivePopups.push(popUpShadow.ids);
-    popUpShadow.listOfUnpinnedPopups.push(popUpShadow.ids);
-    setmousePositionToPopup();
-    popUpShadow.ontheflypopup(request.text)
-    addListenersForDrag();
-  }
+    let json;
+//  if attribute message in request exists, it's a gpt-3 response
+    var id_popup = checkIdPopup(request.id_popup);
+    // check if request.uuid exists
 
-  else if (request.message == 'GPTprompt') {
-    popUpShadow.updatePopupHeader(request, id_popup);
-  }
-  else if (request.message == 'GPTStream_completion') {
-    // split over 'data: ' in case there are multiple streams concatenated
-    var data = request.text.split('data: ');
-    if (popUpShadow.stop_stream == true && popUpShadow.listOfUndesiredStreams.indexOf(request.uuid) == -1) {
-      console.log('Stop stream with uuid',request.uuid)
-      popUpShadow.listOfUndesiredStreams.push(request.uuid);
-      popUpShadow.stop_stream = false;
-      popUpShadow.clearnewlines = true;
+    if (request.message === 'showPopUp') {
+        popUpShadow.ids++; // increment the number of popups, and id of the new popup
+        popUpShadow.listOfActivePopups.push(popUpShadow.ids);
+        popUpShadow.listOfUnpinnedPopups.push(popUpShadow.ids);
+        setMousePositionToPopup();
+        popUpShadow.defaultpopup(); // show the popup
+        addListenersForDrag();
+    } else if (request.message === 'GPTprompt') {
+        popUpShadow.updatePopupHeader(request, id_popup);
+    } else if (request.message === 'GPTStream_completion') {
+        // split over 'data: ' in case there are multiple streams concatenated
+        var data = request.text.split('data: ');
+        if (popUpShadow.stop_stream === true && popUpShadow.listOfUndesiredStreams.indexOf(request.uuid) === -1) {
+            console.log('Stop stream with uuid', request.uuid)
+            popUpShadow.listOfUndesiredStreams.push(request.uuid);
+            popUpShadow.stop_stream = false;
+            popUpShadow.clearnewlines = true;
+        }
+        // if the stream is not in the list of undesired streams, then add it to the popup
+        if (popUpShadow.listOfUndesiredStreams.indexOf(request.uuid) === -1) {
+            for (var m = 1; m < data.length; m++) {// in case of multiple stream in one, loop over them
+                if (data[m].indexOf("[DONE]") === -1) { // if there is not "[DONE]" in the text, it`s a stream
+                    json = JSON.parse(data[m]);
+                    // var stream_id = json.id;
+                    popUpShadow.updatepopup(json, id_popup, true);
+                } else {
+                    popUpShadow.updatepopup(request, id_popup, false);
+                }// the end of the stream, [DONE]
+            }
+        }
+        // in case of error, the split will not produce more than one element
+        if (data.length === 1) {
+            //convert request.text to JSON
+            json = JSON.parse(request.text);
+            if (json.error) {
+                popUpShadow.updatepopup(json, id_popup, true);
+            }
+        }
+    } else {
+        alert(request)
     }
-    // if the stream is not in the list of undesired streams, then add it to the popup
-    if (popUpShadow.listOfUndesiredStreams.indexOf(request.uuid) == -1) 
-    {
-      for (var m = 1; m < data.length; m++) 
-      {// in case of multiple stream in one, loop over them
-        if (data[m].indexOf("[DONE]") == -1) 
-          { // if there is not "[DONE]" in the text, it`s a stream
-            var json = JSON.parse(data[m]);
-            // var stream_id = json.id;
-            popUpShadow.updatepopup(json, id_popup, true);
-          }
-        else 
-          {popUpShadow.updatepopup(request, id_popup, false); }// the end of the stream, [DONE]
-      }
-    }
-    // in case of error, the split will not produce more than one element
-    if (data.length == 1) {
-      //convert request.text to JSON
-      var json = JSON.parse(request.text);
-      if (json.error) {
-        popUpShadow.updatepopup(json, id_popup, true);
-        // addListenersForDrag();
-      }
-    }
-  }
-  else {
-    alert(request)
-  }
 });
-  
-
-
-
 
 
 console.log('GPT-prompter content script is running')
---
+++ content_popup.js
@@ -0,0 +1,415 @@
+const minipopup = (id, {left = 0, top = 0}) => `
+<div class="popuptext" id="${id}" style="left: ${left}px; top:${top}px">
+  <div id="${id}prompt" class="popupprompt">
+  
+    <div id="${id}header" class="grabbable" style='width: 90%;'>
+    </div>
+    <div style='min-width: 120px; width:10%; justify-content: flex-end;'>
+      <button class='minibuttons' id="pin${id}" title="Pin the popup">&#128204;&#xFE0E;</button>
+      <button class='minibuttons' id="minimize${id}" title="Minimize/maximize completion">&#128469;&#xFE0E;</button>
+      <button class='minibuttons' id="mclose${id}" title="Close popup">&#128473;&#xFE0E;</button>
+    </div>
+  </div>
+  <p id="${id}text" class='popupcompletion'></p>
+</div>
+`;
+
+
+const flypopup = (id, {text = "none", left = 0, top = 0}) => `
+<div class="popuptext onylonthefly" id="${id}" style="left: ${left}px; top:${top}px">
+  <div id="${id}prompt" class="popupprompt">
+    <div id="${id}header" class="grabbable" style='width: 90%;'>
+    <b>Prompt on-the-fly</b>: type below and press submit 
+    </div>
+    <div style='min-width: 120px; width:10%; justify-content: flex-end;'>
+      <button class='minibuttons' id="pin${id}">&#128204;&#xFE0E;</button>
+      <button class='minibuttons' id="minimize${id}">&#128469;&#xFE0E;</button>
+      <button class='minibuttons' id="mclose${id}">&#128473;&#xFE0E;</button>
+    </div>
+  </div>
+  <div contentEditable="true" id="${id}textarea" class='textarea'> ${text}</div>
+  <button type="button" id="${id}submit" class="submitbutton">Submit</button>
+  <button type="button" id="${id}stop" class="submitbutton hide" style='background-color: red;'>Stop</button>
+  <p id="${id}text" class='popupcompletion'></p>
+</div>
+`;
+
+
+const styled = `
+  .textarea{
+    border: 1px solid #ffffff;
+    margin-bottom:10px;
+    margin-top:10px;
+  }
+  .grabbable {
+    cursor: move; /* fallback if grab cursor is unsupported */
+    cursor: grab;
+    cursor: -moz-grab;
+    cursor: -webkit-grab;
+  }
+
+  /* (Optional) Apply a "closed-hand" cursor during drag operation. */
+  .grabbable:active {
+    cursor: grabbing;
+    cursor: -moz-grabbing;
+    cursor: -webkit-grabbing;
+  }
+  .submitbutton {
+    background-color: #10a37f;
+    color: white;
+    border: white;
+    border-radius: 5px;
+    padding: 6px 12px;
+    padding-top: 6px;
+    padding-right: 12px;
+    padding-bottom: 6px;
+    padding-left: 12px;
+  }
+  .onylonthefly{
+    border: 2px solid rgb(16, 163, 127);
+  }
+  .popupcompletion {
+    clear: left;
+    cursor: text;
+    white-space: pre-wrap;
+  }
+  .popupprompt {
+    display: flex!important;
+    height: 2em;
+    overflow-y: hidden;
+  }
+  .expand {
+    height: auto;
+  }
+  .popuptext {
+    align-items: center;
+    background-color: #202123;
+    border-radius: 20px;
+    color: #fff;
+    display: block;
+    justify-content:center;
+    opacity:0;
+    position:fixed;
+    width:auto;
+    min-width:200px;
+    max-width:700px;
+    max-height: -webkit-fill-available;
+    z-index:-1;
+    line-height:1.8;
+    // font-size:18px;
+    margin-right:10px!important;
+    font-family: 'Roboto', sans-serif!important;
+    resize:both;
+    overflow:auto;
+  }
+  .show {
+    opacity: 0.9;
+    z-index: 9999;
+    padding: 20px;
+  }
+  .hide {
+    display: none;
+    height: auto;
+  }
+  .resetresize {
+    resize: none!important;
+    height: auto!important;
+    width: auto!important;
+  }
+
+  .minibuttons{
+    color: #fff;
+    background-color: #000;
+    cursor: pointer;
+    margin-left:5px; 
+    font-size:15px;
+    border-radius: 8px;
+  }
+  .invertcolor{
+    color:  #000;
+    background-color:#fff;
+  }
+`;
+
+class popUpClass extends HTMLElement {
+    constructor() {
+        super();
+        this.render();
+    }
+
+    get mousePosition() {
+        return JSON.parse(this.getAttribute("mousePosition") || "{}");
+    }
+
+    static get observedAttributes() {
+        return ["mousePosition"];
+    }
+
+    render() {
+        this.attachShadow({mode: "open"}); // here we create the shadow DOM
+        const style = document.createElement("style");
+        style.textContent = styled;
+        this.shadowRoot.appendChild(style); // here append the style to the shadowRoot
+        this.ids = 0;
+        this.tokens = 0;
+        this.clearnewlines = true;
+        this.listOfActivePopups = [];
+        this.listOfUnpinnedPopups = [];
+        this.listOfUndesiredStreams = [];
+        this.stop_stream = false;
+    }
+
+    //   this function update the style in shadow DOM with the new mousePosition
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === "mousePosition") {
+            if (this.mousePosition.left + 150 > window.innerWidth) {
+                var position = this.mousePosition
+                position.left = window.innerWidth - 150
+                this.lastpop = minipopup(this.ids, position);
+            } else {
+                this.lastpop = minipopup(this.ids, this.mousePosition);
+            }
+        }
+    }
+
+    defaultpopup() {
+        this.shadowRoot.innerHTML += this.lastpop
+        this.shadowRoot.getElementById(this.ids).classList.toggle('show');
+        this.buttonForPopUp();
+    }
+
+    ontheflypopup(selectionText) {
+        this.shadowRoot.innerHTML += flypopup(this.ids, {
+            text: selectionText,
+            left: this.mousePosition.left,
+            top: this.mousePosition.top
+        });
+        this.shadowRoot.getElementById(this.ids).classList.toggle('show');
+        this.buttonForPopUp();
+        const id_textarea = this.shadowRoot.getElementById(this.ids + 'textarea');
+        // stop Propagation of the event for keydown to avoid the background to be selected
+        id_textarea.addEventListener('keydown', (e) => {
+            e.stopPropagation();
+        });
+        this.shadowRoot.getElementById(this.ids + "textarea").focus();
+    }
+
+    pinButtons(id_target, id_button) {
+        this.shadowRoot.getElementById(id_button).addEventListener("click", () => {
+            // if the element is in listOfUnpinnedPopups, remove it from there. If not, add it to the list
+            if (this.listOfUnpinnedPopups.includes(id_target)) {
+                this.listOfUnpinnedPopups.splice(this.listOfUnpinnedPopups.indexOf(id_target), 1);
+
+            } else {
+                this.listOfUnpinnedPopups.push(id_target);
+            }
+            //toggle class to invertcolor
+            this.shadowRoot.getElementById(id_button).classList.toggle('invertcolor');
+        });
+    }
+
+
+    minimizeButtons(id_target, id_button) {
+        this.shadowRoot.getElementById(id_button).addEventListener("click", () => {
+            this.shadowRoot.getElementById(id_target + "text").classList.toggle('hide');
+            this.shadowRoot.getElementById(id_target).classList.toggle('resetresize');
+            // toggle html in minimize button
+            if (this.shadowRoot.getElementById(id_button).innerHTML == "🗕︎") {
+                this.shadowRoot.getElementById(id_button).innerHTML = "&#128470;&#xFE0E;";
+            } else {
+                this.shadowRoot.getElementById(id_button).innerHTML = "&#128469;&#xFE0E;";
+            }
+        });
+    }
+
+    closeButtons(id_target, id_button) {
+        this.shadowRoot.getElementById(id_button).addEventListener("click", () => {
+            this.shadowRoot.getElementById(id_target).classList.toggle('show');
+            this.shadowRoot.getElementById(id_target).remove();
+            this.listOfActivePopups = this.listOfActivePopups.filter(item => item !== id_target);
+        });
+    }
+
+    doubleClick(id_target) {
+        this.shadowRoot.getElementById(id_target).addEventListener("dblclick", () => {
+            this.shadowRoot.getElementById(id_target).classList.toggle('expand');
+        });
+    }
+
+    runClick(id_target) {
+        // if id_target + "submit" has no listener, add one
+        this.shadowRoot.getElementById(id_target + "submit").addEventListener("click", () => {
+            // show stop button and hide run button
+            this.toggleRunStop(id_target);
+
+            this.shadowRoot.getElementById(id_target + "text").innerHTML = "";
+            console.log('Prompt on-the-fly launched from', id_target)
+            var promptDict = {
+                "prompt": this.shadowRoot.getElementById(id_target + "textarea").innerHTML,
+                "model": "text-davinci-003",
+                "temperature": 0.1,
+                "max_tokens": 1000,
+                "popupID": id_target,
+            }
+            chrome.runtime.sendMessage({text: "launchGPT", prompt: promptDict});
+
+        });
+        // make the same listener, but for the ctrl+enter key combination
+        this.shadowRoot.getElementById(id_target + "textarea").addEventListener("keydown", (e) => {
+                if (e.ctrlKey && e.key === 'Enter') {
+                    this.shadowRoot.getElementById(id_target + "submit").click();
+                }
+            }
+        );
+    }
+
+
+    stopButton(id_target) {
+        this.shadowRoot.getElementById(id_target + "stop").addEventListener("click", () => {
+            console.log('Prompt on-the-fly stopped from', id_target)
+            this.stop_stream = true;
+            this.toggleRunStop(id_target);
+
+        });
+        // // make the same listener, but for the ctrl+enter key combination
+        // this.shadowRoot.getElementById(id_target + "textarea").addEventListener("keydown", (e) => {
+        //   if (e.ctrlKey && e.key === 'Enter') {
+        //     this.shadowRoot.getElementById(id_target + "stop").click();
+        //   }
+        // }
+        // );
+    }
+
+    toggleRunStop(id_target) {
+        if (this.shadowRoot.getElementById(id_target + "submit")) {
+            this.shadowRoot.getElementById(id_target + "submit").classList.toggle('hide');
+            this.shadowRoot.getElementById(id_target + "stop").classList.toggle('hide');
+        }
+    }
+
+
+    buttonForPopUp() {
+        for (var i = 0; i < popUpShadow.listOfActivePopups.length; i++) {
+            //
+            var id_target = popUpShadow.listOfActivePopups[i];
+            // const id_target =this.ids
+            const id_pin = "pin" + id_target;
+            const id_close = "mclose" + id_target;
+            const id_minimize = "minimize" + id_target;
+            this.pinButtons(id_target, id_pin);
+            this.minimizeButtons(id_target, id_minimize);
+            this.closeButtons(id_target, id_close);
+            this.doubleClick(id_target + "prompt");
+            if (this.shadowRoot.getElementById(id_target + "submit")) {
+                this.runClick(id_target);
+                this.stopButton(id_target);
+            }
+
+        }
+        ;
+    }
+
+
+    updatePopupHeader(request, target_id) {
+        this.shadowRoot.getElementById(target_id + "header").innerHTML = "<i> " + request.text + "</i>";
+        // if (this.shadowRoot.getElementById("regenerate" + target_id)) {
+        //   this.regenerateButton(target_id, request);
+        // }
+    }
+
+    //to be finished
+    regenerateButton(id_target, request) {
+        this.shadowRoot.getElementById("regenerate" + id_target).addEventListener("click", () => {
+            this.shadowRoot.getElementById(id_target + "text").innerHTML = "";
+            console.log('Prompt on-the-fly launched from', id_target)
+            var promptDict = {
+                "prompt": request.text,
+                "model": request.body_data.model,
+                "temperature": request.body_data.temperature,
+                "max_tokens": request.body_data.max_tokens,
+                "popupID": id_target,
+            }
+            chrome.runtime.sendMessage({text: "launchGPT", prompt: promptDict});
+
+        });
+    };
+
+    copyToClipboard = async (text) => {
+        try {
+            await navigator.clipboard.writeText(text);
+            console.log('Text copied to clipboard');
+        } catch (err) {
+            console.error('Failed to copy: ', err);
+        }
+    };
+
+    updatepopup(message, target_id, stream) {
+        // console.log('updatepopup', message, target_id, stream)
+        //if stream is true
+        if (stream) {
+            // if choices is a key in message, it means usual stream
+            if (message.choices) {
+                var text = message.choices[0].text
+                // if self.tokens is the first or second and text is a new line character, we don't add it
+                if (this.clearnewlines && text == "\n") {
+                    // console.log('new line \\n skipped from GPT stream')
+                    return
+                } else {
+                    this.clearnewlines = false;
+                    this.shadowRoot.getElementById(target_id + "text").innerHTML += text;
+                }
+            }
+            // if message has a key "error"
+            else if (message.error) {
+                var text = message.error.message
+                var type = message.error.type
+                this.shadowRoot.getElementById(target_id + "text").innerHTML += type + "<br>" + text;
+                this.tokens = 0;
+                //show run button and hide stop button
+                this.toggleRunStop(target_id);
+            }
+            // each message should be 1 token
+            this.tokens++;
+
+        } else {
+            // show run button and hide stop button
+            this.toggleRunStop(target_id);
+            var complete_completion = this.shadowRoot.getElementById(target_id + "text").innerHTML
+            // add a button to copy the text to clipboard
+            this.addCopyToClipboardBtn(target_id, complete_completion);
+
+            //save prompt to local storage
+
+            var body_data = JSON.parse(message.body_data)
+            var model = body_data.model
+            this.clearnewlines = true;
+            this.tokens = 0;
+            // save the result.choices[0].text in the storage
+            chrome.storage.local.get('history', function (items) {
+                if (typeof items.history !== 'undefined') {
+                    items.history.push([message.body_data, complete_completion, 0]);// add the result to the history
+                    chrome.storage.local.set({'history': items.history});
+                } else {
+                    items.history = [[message.body_data, complete_completion, 0]]; // initialize the history array
+                    chrome.storage.local.set({'history': items.history});
+                }
+            });
+        }
+    }
+
+    addCopyToClipboardBtn(target_id, complete_completion) {
+        this.shadowRoot.getElementById(target_id + "text").innerHTML += "<button class='minibuttons' id='copy_to_clipboard" + target_id + "' title='Copy to clipboard'>&#x2398;&#xFE0E;</button>"; //
+        this.shadowRoot.getElementById("copy_to_clipboard" + target_id).addEventListener("click", () => {
+            this.copyToClipboard(complete_completion);
+            // invert color for 1 second
+            this.shadowRoot.getElementById("copy_to_clipboard" + target_id).classList.toggle('invertcolor');
+            setTimeout(() => {
+                this.shadowRoot.getElementById("copy_to_clipboard" + target_id).classList.toggle('invertcolor');
+            }, 500);
+        });
+    }
+}
+
+window.customElements.define("mini-popup", popUpClass);
+
+
